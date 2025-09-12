ecb15596b2d9652bf1a46114d6bcce7f9c946e73
mayconsantos <mv.maycon.araujo.santos@gmail.com>
2025-09-12 16:34:13 -0300

    refactor: change values from chatpro information

--- app/src/main/kotlin/com/zecobranca/main/config/Env.kt
+++ app/src/main/kotlin/com/zecobranca/main/config/Env.kt
@@ -1,8 +1,8 @@
 package com.zecobranca.main.config
 
 object Env {
-    val chatProApiToken: String = System.getenv("CHATPRO_API_TOKEN") ?: "aeb9d7999bbcf9879caed8de1ffb6311"
-    val chatProInstanceId: String = System.getenv("CHATPRO_INSTANCE_ID") ?: "chatpro-j7phcrjlmn"
+    val chatProApiToken: String = System.getenv("CHATPRO_API_TOKEN") ?: "cd98f539046023b02d5cab608bfd3457"
+    val chatProInstanceId: String = System.getenv("CHATPRO_INSTANCE_ID") ?: "chatpro-1vqosd3o0b"
     val chatProApiUrl: String = System.getenv("CHATPRO_API_URL") ?: "https://v5.chatpro.com.br/"
     val port: Int = System.getenv("PORT")?.toIntOrNull() ?: 8080
 }
